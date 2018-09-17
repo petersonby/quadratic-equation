6374fa95ba6fd58f14b552c2b4fb21b88395bf97
Peterson <contact@peterson.by>
2018-09-17 14:42:04 +0300

    solveEquation function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const numbers = equation.match(/\-?[\-?\+?]? ?\d+/g);
+  const a = parseInt(numbers[0], 10);
+  const b = parseInt(numbers[2].replace(' ', ''), 10);
+  const c = parseInt(numbers[3].replace(' ', ''), 10);
+  const d = Math.pow(b, 2) - 4 * a * c;
+  const x = [];
+  
+  if (d >= 0) {
+  	let x1 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+  	let x2 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+  	x.push(x1);
+  	x.push(x2);
+  }
+
+  x.sort((a,b) => a-b);
+  return x;
 }
